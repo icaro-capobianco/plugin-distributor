c734e3ee3f86dbaa8d82857af8c2dc72922f21d0
Icaro C. Capobianco <icaro.capobianco@gmail.com>
2020-06-23 12:39:24 -0300

    Attempting to ignore user abort

--- git-deploy/deployer.php
+++ git-deploy/deployer.php
@@ -8,16 +8,46 @@ $sha     = false;
 $DIR     = preg_match("/\/$/", DIR) ? DIR : DIR . "/";
 
 
-ignore_user_abort(true);
-set_time_limit(MAX_EXECUTION_TIME);
-ob_start();
-echo "Request being processed";
-ob_end_flush();
-ob_flush();
-flush();
-session_write_close();
-fastcgi_finish_request();
+function respondOK($text = null) {
+    // check if fastcgi_finish_request is callable
+    if (is_callable('fastcgi_finish_request')) {
+        if ($text !== null) {
+            echo $text;
+        }
+        /*
+         * http://stackoverflow.com/a/38918192
+         * This works in Nginx but the next approach not
+         */
+        session_write_close();
+        fastcgi_finish_request();
+ 
+        return;
+    }
+ 
+    ignore_user_abort(true);
+ 
+    ob_start();
+ 
+    if ($text !== null) {
+        echo $text;
+    }
+ 
+    $serverProtocol = filter_input(INPUT_SERVER, 'SERVER_PROTOCOL', FILTER_SANITIZE_STRING);
+    header($serverProtocol . ' 200 OK');
+    // Disable compression (in case content length is compressed).
+    header('Content-Encoding: none');
+    header('Content-Length: ' . ob_get_length());
+ 
+    // Close the connection.
+    header('Connection: close');
+ 
+    ob_end_flush();
+    ob_flush();
+    flush();
+}
 
+
+respondOK( "Processing request" );
 
 // retrieve the token
 if (!$token && isset($_SERVER["HTTP_X_HUB_SIGNATURE"])) {
